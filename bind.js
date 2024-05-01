942eab05801a5e4f478ceed62771e4df68e450f4
Lintee12 <lintonevan@gmail.com>
2024-05-01 10:20:08 -0300

    update

--- bind.js
+++ bind.js
@@ -1,6 +1,7 @@
 "use strict";
 class Bind {
 	constructor() {
+		this.rootElement = null;
 		this.initialDOM = [];
 		this.currentDOM = [];
 		this.lastUpdate = {};
@@ -23,33 +24,49 @@ class Bind {
 		return "{{" + name + "}}";
 	}
 
-	render(root, app, callback) {
-		const rootElement = document.querySelector(root);
-		this.initialDOM = Array.from(rootElement.cloneNode(true).getElementsByTagName("*"));
-		this.currentDOM = Array.from(rootElement.getElementsByTagName("*"));
+	getElement = (selector) => {
+		return this.rootElement.querySelector(selector);
+	};
+
+	bind = (selector, event, func) => {
+		const element = this.getElement(selector);
+		element.addEventListener(event, func);
+	};
+
+	render(root, { properties, methods }, callback) {
+		this.rootElement = document.querySelector(root);
+		this.initialDOM = Array.from(this.rootElement.cloneNode(true).getElementsByTagName("*"));
+		this.currentDOM = Array.from(this.rootElement.getElementsByTagName("*"));
 
 		if (typeof callback === "function") callback();
 
-		this.updateDOM(app);
+		this.updateDOM(properties);
 
 		setInterval(() => {
-			for (let i = 0; i < this.getVarLength(app); i++) {
-				const data = this.getVarData(app, i);
+			for (let i = 0; i < this.getVarLength(properties); i++) {
+				const data = this.getVarData(properties, i);
 				if (this.lastUpdate[i] && this.lastUpdate[i].value !== data.value) {
-					console.log(`updating ${data.name}`);
-					this.updatePropertyInDOM(app, data.name);
+					this.updatePropertyInDOM(properties, data.name);
 				}
 				this.lastUpdate[i] = {
 					value: data.value,
 				};
 			}
 		}, 0);
+
+		if (methods) {
+			for (const methodName in methods) {
+				if (Object.hasOwnProperty.call(methods, methodName)) {
+					methods[methodName]();
+				}
+			}
+		}
 	}
 
-	updatePropertyInDOM(app, property) {
+	updatePropertyInDOM(properties, property) {
 		this.initialDOM.forEach((element, index) => {
-			for (let i = 0; i < this.getVarLength(app); i++) {
-				const data = this.getVarData(app, i);
+			for (let i = 0; i < this.getVarLength(properties); i++) {
+				const data = this.getVarData(properties, i);
 				if (element.innerHTML.includes(this.htmlValue(property))) {
 					if (element.innerHTML.includes(this.htmlValue(data.name)) && element.children.length === 0) {
 						this.currentDOM[index].innerHTML = element.innerHTML.replace(this.htmlValue(data.name), data.value);
@@ -59,10 +76,10 @@ class Bind {
 		});
 	}
 
-	updateDOM(app) {
+	updateDOM(properties) {
 		this.initialDOM.forEach((element, index) => {
-			for (let i = 0; i < this.getVarLength(app); i++) {
-				const data = this.getVarData(app, i);
+			for (let i = 0; i < this.getVarLength(properties); i++) {
+				const data = this.getVarData(properties, i);
 				if (element.innerHTML.includes(this.htmlValue(data.name)) && element.children.length === 0) {
 					this.currentDOM[index].innerHTML = element.innerHTML.replace(this.htmlValue(data.name), data.value);
 				}
